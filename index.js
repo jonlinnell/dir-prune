56bc63c7e0416f65ef208cb5146a3746dbb4042b
Jon Linnell <linnell.jon@gmail.com>
2019-01-11 10:24:27 +0000

    Add business days instead of calendar days

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const moment = require('moment')
+const { addWeekDays } = require('moment-business')
 
-const { MAXAGE, TARGET } = process.env
+const { MAXAGE, TARGET, VERBOSE } = process.env
 
 const omitFiles = ['logs', 'readme.txt']
 
@@ -19,12 +20,12 @@ const target = path.resolve(TARGET)
 
 const logDeletion = file => fs.writeFile(
   path.join(target, 'logs', `${moment().format('YYYYMMDD')}.log`),
-  `[${moment().format('HH:mm:ss')}] ${file} deleted.\n`,
+  `[${moment().format('HH:mm:ss')}] ${file} deleted.`,
   {
     encoding: 'utf8',
     flag: 'a+',
   },
-  err => { if (err) { process.stderr.write(err) } }
+  err => { if (err) { console.error(err) } }
 )
 
 fs.readdir(target, (error, files) => {
@@ -35,16 +36,22 @@ fs.readdir(target, (error, files) => {
       const resolvedFile = path.join(TARGET, file)
 
       const modifiedDate = moment(fs.statSync(resolvedFile).mtime)
-      const deleteDate = moment(modifiedDate).add(MAXAGE, 'days')
+      const deleteDate = moment(modifiedDate)
+      addWeekDays(deleteDate.startOf('day'), MAXAGE)
+
+      if (VERBOSE) { console.log(`File "${file}", modified at ${modifiedDate}, due to be deleted on ${deleteDate}.`) }
 
       if (deleteDate < moment()) {
         fs.unlink(resolvedFile, (unlinkError) => {
           if (unlinkError) {
-            process.stderr.write(`There was an error: ${unlinkError}.\n`)
+            console.error(`There was an error: ${unlinkError}.`)
           } else {
+            if (VERBOSE) { console.log(`File "${file}" deleted.`) }
             logDeletion(file)
           }
         })
+      } else {
+        if (VERBOSE) { console.log(`Skipping file "${file}"; too early to delete.`) }
       }
     }
   })
